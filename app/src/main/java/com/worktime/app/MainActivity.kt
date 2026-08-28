437a0f2c0f69f0d38ff75e69375cd8da05b84c84
arttvad9r <arttvad9r@gmail.com>
2026-08-29 00:37:11 +0300

    fix(motion): remove splash view safely

--- app/src/main/java/com/worktime/app/MainActivity.kt
+++ app/src/main/java/com/worktime/app/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
             provider.view.animate()
                 .alpha(0f)
                 .setDuration(160L)
-                .withEndAction(provider::remove)
+                .withEndAction { provider.remove() }
                 .start()
         }
 
