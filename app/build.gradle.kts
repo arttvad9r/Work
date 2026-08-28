2c1e60c0b146b201fd1f336a4edbd896057c7c74
arttvad9r <arttvad9r@gmail.com>
2026-08-29 01:30:13 +0300

    build: depend on compose foundation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.lifecycle.runtime.compose)
     implementation(libs.compose.ui)
+    implementation(libs.compose.foundation)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
     implementation(libs.compose.material.icons.core)
