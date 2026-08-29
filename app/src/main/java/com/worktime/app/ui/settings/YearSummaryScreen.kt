c69dbfe5f326d76254b032000b08e06a8c3b7e74
arttvad9r <arttvad9r@gmail.com>
2026-08-29 23:29:06 +0300

    fix: import year summary status bar padding

--- app/src/main/java/com/worktime/app/ui/settings/YearSummaryScreen.kt
+++ app/src/main/java/com/worktime/app/ui/settings/YearSummaryScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.rememberPagerState
