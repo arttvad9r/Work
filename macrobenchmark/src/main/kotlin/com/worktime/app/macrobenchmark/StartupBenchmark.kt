0156c5b4d111351b5042d8d52337bffaa883709a
arttvad9r <arttvad9r@gmail.com>
2026-08-30 12:54:19 +0300

    fix: import macrobenchmark junit rule

--- macrobenchmark/src/main/kotlin/com/worktime/app/macrobenchmark/StartupBenchmark.kt
+++ macrobenchmark/src/main/kotlin/com/worktime/app/macrobenchmark/StartupBenchmark.kt
@@ -1,9 +1,9 @@
 package com.worktime.app.macrobenchmark
 
 import androidx.benchmark.macro.CompilationMode
-import androidx.benchmark.macro.MacrobenchmarkRule
 import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import org.junit.Rule
