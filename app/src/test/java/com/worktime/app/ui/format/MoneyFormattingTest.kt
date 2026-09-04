c24194568a222724e2ec4c8fdf4096fb8516fb8e
arttvad9r <arttvad9r@gmail.com>
2026-09-04 14:53:27 +0300

    test: cover stable calendar amount grouping

--- app/src/test/java/com/worktime/app/ui/format/MoneyFormattingTest.kt
+++ app/src/test/java/com/worktime/app/ui/format/MoneyFormattingTest.kt
@@ -44,9 +44,10 @@ class MoneyFormattingTest {
     }
 
     @Test
-    fun `whole amount formatter rounds away calendar fractions and groups thousands`() {
-        assertEquals("1,235", formatWholeAmountMicros(1_234_500_000L, Locale.US))
-        assertEquals("1,234", formatWholeAmountMicros(1_234_499_999L, Locale.US))
-        assertEquals("4,810", formatWholeAmountMicros(4_810_000_000L, Locale.US))
+    fun `whole amount formatter rounds calendar fractions and uses stable space grouping`() {
+        assertEquals("1 235", formatWholeAmountMicros(1_234_500_000L, Locale.US))
+        assertEquals("1 234", formatWholeAmountMicros(1_234_499_999L, Locale.US))
+        assertEquals("4 810", formatWholeAmountMicros(4_810_000_000L, Locale.US))
+        assertEquals("4 810", formatWholeAmountMicros(4_810_000_000L, Locale("ru")))
     }
 }
